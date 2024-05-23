43307ecd18414a355894e4ad50796243d1413f02
Mark Andal <andalghian14@gmail.com>
2024-05-23 10:33:42 +1000

    v1.8 about file added
    about file added - mark

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sparefood</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS file for styling -->
+         
+
+</head>
+<div  class="logo">
+            <a href="index.php"><h1>Sparefood</h1></a>
+
+<body>
+    
+    </div>
+
+    <div class="about">
+        <a href="about"><h3>About Us</h3></a>
+    </div>
+
+    <header>
+        
+        <nav>
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Vegetables</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    </footer>
+</body>
+</html>
