3c4e34dbb3d62615aec5f04e0d3c8eb444254a4b
Mark Andal <andalghian14@gmail.com>
2024-05-31 16:52:32 +1000

    v2.2
    Database connected and add food function working

--- addfood.php
+++ addfood.php
@@ -1,3 +1,37 @@
+<?php include 'connection.php';
+if (isset($_POST['add_food'])){
+
+    $food_name = $_POST['food_name'];
+    $expiry_date = $_POST['expiry_date'];
+    $address = $_POST['address'];
+    $file_name = $_FILES['file_name']['name'];
+    $temp_name = $_FILES['file_name']['tmp_name'];
+    $file_name_folder = 'images/'.$file_name;
+
+
+    $insert_query = mysqli_query($conn, "insert into foods (name, date, address, image) values ('$food_name', '$expiry_date', '$address', '$file_name')")  or die("insert failed");
+
+
+   if ($insert_query){
+    move_uploaded_file($temp_name, $file_name_folder);
+
+if ($insert_query) {
+    $message = "Operation was successful!";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+} else {
+    $message = "Operation failed!";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+}
+
+}
+}
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +39,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sparefood</title>
     <link rel="stylesheet" href="style.css"> <!-- Link to your CSS file for styling -->
+
+    <?php include 'connection.php' ?>
          
 
 </head>
@@ -35,11 +71,12 @@
 
     <div class="form_container">
             <h3 class="heading">Add Food</h3>
-            <form action="" class="add_food">
-                <input type="text" placeholder="Enter food name" class="input_field" required>
-                <input type="number" min= "0"placeholder="Days until expiry" class="input_field" required>
-                <input type="file" class="input_field" required>
-                <input type="submit" class="submit" value="Add Food">
+            <form action="" class="add_food" method="post" enctype="multipart/form-data">
+                <input type="text" name="food_name" placeholder="Enter food name" class="input_field" required>
+                <input type="number" name="expiry_date" min= "0"placeholder="Days until expiry" class="input_field" required>
+                <input type="text" name="address" placeholder="Enter your address" class="input_field" required>
+                <input type="file" name="file_name" class="input_field">
+                <input type="submit" name="add_food" class="submit" value="Add Food">
             </form>
     </div>
 
--- cart.php
+++ cart.php
@@ -11,34 +11,20 @@
 <div  class="logo">
             <a href="index.php"><h1>Sparefood</h1></a>
 
-<body>
-
-    
-
-    
-    </div>
-
-    <div class="about">
-        <div class="cart">
-        
-    </div>
-
-    <header>
+            <header>
         
         <nav class="nav_bar">
             <ul>
                 <li><a href="addfood.php">Add Food</a></li>
                 <li><a href="cart.php">Cart</a></li>
-                <li><a href="about.php">About Us</a></li>
+               <li><a href="about.php">About Us</a></li>
             </ul>
         </nav>
     </header>
 
-    <section style= "border: solid;" style="text-align: center;">
-        
-        <h2 style= "border: double;">Cart</h2>
-            <p style="text-align: center;">Cart empty</p>
-    </section>
+<body>
+
+
 
     
 </body>
